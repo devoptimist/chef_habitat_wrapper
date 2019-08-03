16606a17df51932387549b5c249bf7cd7fc528e2
devoptimist <sbrown@chef.io>
2019-08-03 03:37:28 +0100

    update to plan.ps1 file

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -4,8 +4,7 @@ $pkg_version="0.83.0"
 $pkg_upstream_url="http://chef.io"
 $pkg_build_deps=@("core/chef-dk")
 $pkg_deps=@(
-  "core/cacerts"
   "stuartpreston/chef-client" # https://github.com/habitat-sh/habitat/issues/6671
 )
-$pkg_scaffolding="chef/scaffolding-chef-infra"
-$scaffold_policy_name="base"
+$scaffold_policy_name="Policyfile"
+$scaffold_policyfile_path="$PLAN_CONTEXT/../" # habitat/../Policyfile.rb
